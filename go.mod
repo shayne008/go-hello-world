3e3dc84201d4c676074ec6a251c6b97a49da7dd2
Heather <hthg.02@gmail.com>
2024-03-17 20:53:40 +0000

    Intial Go commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shayne008/go-hello-world
+
+go 1.21.8
---
+++ main.go
@@ -0,0 +1,9 @@
+package main                   // The name of your package
+
+import (                       // Import other packages and modules
+  "fmt"                        // Import the fmt package
+)
+
+func main() {                  // The main function, that will always run
+  fmt.Println("Hello, World!") // Using the fmt package to print
+}
